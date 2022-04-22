87ee41a782e4d879456a86bfcd073947eb63f847
hopbv <hopbuivan@tech.admicro.vn>
2022-04-22 11:28:07 +0700

    convert onnx and predictor for onnx

---
+++ convert_model_to_onnx.py
@@ -0,0 +1,51 @@
+import torch
+import hydra
+import logging
+
+from omegaconf.omegaconf import OmegaConf
+
+from model import ColaModel
+from data import DataModule
+
+logger = logging.getLogger(__name__)
+
+@hydra.main(config_path="./configs", config_name="config")
+def convert_model(cfg):
+    root_dir = hydra.utils.get_original_cwd()
+    model_path = f"{root_dir}/models/best.ckpt"
+    logger.info(f"Loading pre-trained model from : {model_path}")
+    cola_model = ColaModel.load_from_checkpoint(model_path)
+    data_model = DataModule(
+        cfg.model.tokenizer, cfg.processing.batch_size, cfg.processing.max_length
+    )
+    data_model.prepare_data()
+    data_model.setup()
+    input_batch = next(iter(data_model.train_dataloader()))
+    input_sample = {
+        "input_ids": input_batch["input_ids"][0].unsqueeze(0),
+        "attention_mask": input_batch["attention_mask"][0].unsqueeze(0)
+    }
+    logger.info(f"Converting the model into ONNX format")
+    torch.onnx.export(
+        cola_model,
+        (
+            input_sample["input_ids"],
+            input_sample["attention_mask"]
+        ),
+        f"{root_dir}/models/model.onnx",
+        export_params=True,
+        opset_version=10,
+        input_names=["input_ids", "attention_mask"],
+        output_names=["output"],
+        dynamic_axes={
+            "input_ids": {0: "batch_size"},
+            "attention_mask": {0: "batch_size"},
+            "output": {0: "batch_size"}
+        }
+    )
+    logger.info(
+        f"Model converted successfully. ONNX format model is at: {root_dir}/models/model.onnx"
+    )
+
+if __name__ == '__main__':
+    convert_model()
--- inference_onnx.py
+++ inference_onnx.py
@@ -1,27 +1,26 @@
-import torch
-from model import ColaModel
+import numpy as np
+import onnxruntime as ort
+from scipy.special import softmax
+
 from data import DataModule
 from utils import timing
 
-class ColaPredictor:
+class ColaONNXPredictor:
     def __init__(self, model_path):
-        self.model_path = model_path
-        self.model = ColaModel.load_from_checkpoint(model_path)
-        self.model.eval()
-        self.model.freeze()
+        self.ort_session = ort.InferenceSession(model_path)
         self.processor = DataModule()
-        self.softmax = torch.nn.Softmax(dim=0)
         self.labels = ["unacceptable", "acceptable"]
 
     @timing
     def predict(self, text):
         inference_sample = {"sentence": text}
         processed = self.processor.tokenize_data(inference_sample)
-        logits = self.model(
-            torch.tensor([processed["input_ids"]]),
-            torch.tensor([processed["attention_mask"]]),
-        )
-        scores = self.softmax(logits[0]).tolist()
+        ort_inputs = {
+            "input_ids": np.expand_dims(processed["input_ids"], axis=0),
+            "attention_mask": np.expand_dims(processed["attention_mask"], axis=0)
+        }
+        ort_outputs = self.ort_session.run(None, ort_inputs)
+        scores = softmax(ort_outputs[0])[0]
         predictions = []
         for score, label in zip(scores, self.labels):
             predictions.append({"label": label, "score": score})
@@ -29,7 +28,7 @@ class ColaPredictor:
 
 if __name__ == '__main__':
     sentence = "The boy is sitting on a bench"
-    predictor = ColaPredictor("./models/best.ckpt")
+    predictor = ColaONNXPredictor("./models/model.onnx")
     print(predictor.predict(sentence))
     sentences = ["The boy is sitting on a bench"] * 10
     for sentence in sentences:
